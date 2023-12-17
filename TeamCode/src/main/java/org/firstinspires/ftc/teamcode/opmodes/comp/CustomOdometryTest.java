9ca115bd6d1f4b42d6a8398bb4fafc9771b7294c
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-12-16 18:06:04 -0800

    Experimental Strafe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/comp/CustomOdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/comp/CustomOdometryTest.java
@@ -7,14 +7,13 @@ import org.firstinspires.ftc.teamcode.customclasses.Clock;
 import org.firstinspires.ftc.teamcode.customclasses.PIDController;
 import org.firstinspires.ftc.teamcode.opmodes.WaitingAutoLinear;
 
-@Autonomous(name = "staytest")
+@Autonomous(name = "Custom Odometry Test")
 public class CustomOdometryTest extends WaitingAutoLinear {
     private static final double MASTER_FORWARD_SPEED = 0.5;
     private static final double TICKS_PER_REVOLUTION = 8134;
     private static final double WHEEL_RADIUS = 0.688975;// in inches
     private static final double INCHES_PER_TICK = 2.0 * Math.PI  * WHEEL_RADIUS / TICKS_PER_REVOLUTION;
     private final Clock detectionTime = new Clock();
-
     private int initialLeft,initialRight;
 
     @Override
@@ -23,16 +22,17 @@ public class CustomOdometryTest extends WaitingAutoLinear {
     @Override
     public void run() {
         forward(24,10);
-
-
+        sleep(1_000);
     }
 
     private void forward(double inches, double seconds) {
-        final double POSITION_THRESHOLD = 0.1;
+        final double POSITION_THRESHOLD = 0.1; // to make it always wait for <seconds> make this value negative
         final PIDController strafeFixer = new PIDController(0.00005,0.000001,0);
         int initial_encoder = robot.rightBack.getCurrentPosition();
         resetRelatives();
-        double forwardError,strafeError, turnError;
+        double forwardError;
+        double strafeError;
+        double turnError;
         detectionTime.reset();
         do {
             strafeError = (robot.rightBack.getCurrentPosition() - initial_encoder) * INCHES_PER_TICK;
@@ -45,12 +45,38 @@ public class CustomOdometryTest extends WaitingAutoLinear {
             robot.emulateController(Math.tanh(forwardError)*MASTER_FORWARD_SPEED, strafeFixer.Update(strafeError), turnError/ 10);
             telemetry.update();
         } while (detectionTime.getTimeSeconds() < seconds && Math.sqrt(forwardError*forwardError + strafeError *strafeError + turnError*turnError) > POSITION_THRESHOLD);
-
+        robot.stop();
+        telemetry.addData("Finished in: ",detectionTime.getTimeSeconds());
+        telemetry.update();
     }
-
+    private void strafe(double inchesRight, double seconds) {
+        final double POSITION_THRESHOLD = 0.1; // to make it always wait for <seconds> make this value negative
+        final PIDController forwardFixer = new PIDController(0.5,0.0001,0);
+        int initial_encoder = robot.rightBack.getCurrentPosition();
+        resetRelatives();
+        double forwardError;
+        double strafeError;
+        double turnError;
+        detectionTime.reset();
+        do {
+            double strafeRelative =  (robot.rightBack.getCurrentPosition() - initial_encoder) * INCHES_PER_TICK;
+            strafeError = inchesRight - strafeRelative;
+            int avg = ((getRelativeLeft())-(getRelativeRight()) )/2;
+            double forwardRelative = avg * INCHES_PER_TICK ;
+            telemetry.addData("strafeRel stra",strafeRelative);
+            telemetry.addData("Time Left",seconds - detectionTime.getTimeSeconds());
+            forwardError =  forwardRelative;
+            turnError = (getRelativeLeft() - getRelativeLeft()) * INCHES_PER_TICK;
+            robot.emulateController(Math.tanh(forwardError)*MASTER_FORWARD_SPEED, forwardFixer.Update(strafeError), turnError/ 10);
+            telemetry.update();
+        } while (detectionTime.getTimeSeconds() < seconds && Math.sqrt(forwardError*forwardError + strafeError *strafeError + turnError*turnError) > POSITION_THRESHOLD);
+        robot.stop();
+        telemetry.addData("Finished in: ",detectionTime.getTimeSeconds());
+        telemetry.update();
+    }
     private void turnLeft90() {
         robot.emulateController(0, 0, -.5);
-        sleep(1_200);
+        sleep(1_100);
         robot.stop();
     }
     public int getRelativeLeft() {
