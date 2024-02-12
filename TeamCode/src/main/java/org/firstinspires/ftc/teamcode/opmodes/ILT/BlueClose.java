19c2cccf8bdbb2d7bdd8e38b3d8b2d1e966e1289
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2024-02-12 13:56:10 -0800

    fix2.71828182845

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ILT/BlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ILT/BlueClose.java
@@ -111,14 +111,13 @@ public class BlueClose extends WaitingAuto {
 
         switch (detection) {
             case LEFT:
-                bob.turn(Math.PI/2)
+                return bob.turn(Math.PI/2)
                     .back(5)
                     .waitSeconds(3)
                     .splineTo(new Vector2d(-45,-50) ,Math.PI)
                     .setReversed(false)
                     //.forward(12)
-                    .strafeTo(new Vector2d(-40,-36));
-                break;
+                    .strafeTo(new Vector2d(-40,-36)).build();
             case CENTER:
                 bob.back(3)
                     .addTemporalMarker(() -> pixelQuickRelease.setState(MechanismState.OPEN))
